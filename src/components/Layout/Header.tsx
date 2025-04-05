7324d83ddcad519ca6c7187101aa4f11117743a4
newbialywhodis <konradbialy16@gmail.com>
2025-04-05 18:17:03 +0200

    Poprawki dla mobilnych

--- src/components/Layout/Header.tsx
+++ src/components/Layout/Header.tsx
@@ -136,6 +136,7 @@ export function Header() {
                 aria-label="Change language"
                 checkIconPosition="right"
                 renderOption={renderSelectOption}
+                comboboxProps={{ zIndex: 1000001 }}
             />
             <Group justify='center' mt="md">
                 {ThemeSwitcher}
