51b207d9a0542f15886d059e205ad2a5efe199be
newbialywhodis <konradbialy16@gmail.com>
2025-04-05 18:02:31 +0200

    juz chyba powinno smigac xddd

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,9 +1,6 @@
-import { getTranslations, setRequestLocale } from 'next-intl/server';
-import { Container, Title, Text, Button, SimpleGrid, Stack, Group, ThemeIcon, Paper, Anchor, List, ListItem } from '@mantine/core';
-import { IconCircleCheck, IconMapPin, IconPhone } from '@tabler/icons-react';
-
+import { Container } from '@mantine/core';
+import { setRequestLocale } from 'next-intl/server';
 import { MapComponent } from '@/components/Shared/MapComponent';
-import { ContactForm } from '@/components/Contact/ContactForm';
 import { HeroSection } from '@/components/Home/HeroSection';
 import { ServicesSection } from '@/components/Home/ServicesSection';
 import { ContactSection } from '@/components/Contact/ContactSection';
@@ -14,12 +11,7 @@ type Props = {
 
 export default async function HomePage({ params }: Props) {
   const { locale } = await params;
-
   setRequestLocale(locale);
-
-  const tContact = await getTranslations('ContactDetails');
-  const tFooter = await getTranslations('Footer');
-  const tForm = await getTranslations('ContactForm');
 
   return (
     <>
